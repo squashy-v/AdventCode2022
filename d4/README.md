f5ddc927f191e6eb3910f61b78f0dc4d8519544a
squashy <jjamesvandergriff@yahoo.com>
2022-12-20 03:05:20 -0500

    d4

---
+++ d4/README.md
@@ -0,0 +1,30 @@
+# Advent of Code 2022
+## Day 4
+https://adventofcode.com/2022/day/4
+
+
+### Part 1
+Task: In how many assignment pairs does one range fully contain the other?
+
+
+Example Input: 
+
+---
+```
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+```
+---
+
+
+After some dirty looping through the list of data eventually created a 2d list that contains a list of each pairing of elf's assignments and the pairings in format `[[2,4],[6,8]]`. Defined a function to check if a list contained another and wrote a for loop to count how many time the function returned true, and had to pass each pair in twice, to compensate for the fact that the second assignment can contain the first as well.
+
+
+
+### Part 2
+
+Now just checking if the bounds of the first assignment surrounds the lowerbound of the second assignement, or checking if the bounds of the first assignment surround the upperbound of the second assignment returns true, and once again checks both assignments on each other.
---
+++ d4/cleanup.py
@@ -0,0 +1,55 @@
+FILENAME = "cleanup.input"
+
+def generate_list():
+    with open(FILENAME, "rt", encoding="ascii") as input_file:
+        reader = input_file.read().split("\n")
+        elf_assignment = [x.split(',') for x in reader]
+        for x in elf_assignment:
+            for i in range(len(x)):
+                x[i] = x[i].split("-")
+        return elf_assignment
+
+def does_contain(x: list, y: list) -> bool:
+    if (int(x[0]) <= int(y[0]) and int(x[1]) >= int(y[1])):
+        return True
+    else:
+        return False
+
+
+def any_contain(x: list, y:list) -> bool:
+    if (int(x[0]) <= int(y[0]) and int(x[1]) >= int(y[0])):
+        return True
+    elif (int(x[0]) <= int(y[1]) and int(x[0]) >= int(y[1])):
+        return True
+    else:
+        return False
+
+
+def part_one():
+    elf_assignment = generate_list()
+    
+    count = 0
+    for x in elf_assignment:
+        if does_contain(x[0], x[1]):
+            count += 1
+        elif does_contain(x[1], x[0]):
+            count += 1
+    print(count)
+
+
+def part_two():
+    elf_assignment = generate_list()
+    count = 0
+    for x in elf_assignment:
+        if any_contain(x[0], x[1]):
+            count += 1
+        elif any_contain(x[1], x[0]):
+            count += 1
+    print(count)
+    
+
+
+
+if __name__ == "__main__":
+    part_one()
+    part_two()
