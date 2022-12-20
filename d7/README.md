9b8447c43edd1b537c4c612dfdd94506f1955ae0
squashy <jjamesvandergriff@yahoo.com>
2022-12-20 03:05:55 -0500

    d7

---
+++ d7/README.md
@@ -0,0 +1,39 @@
+# Advent of Code 2022
+## Day 7
+https://adventofcode.com/2022/day/7
+
+
+### Part 1
+Task: Find all of the directories with a total size of at most 100000. What is the sum of the total sizes of those directories?
+
+
+Example Input:
+
+```
+---
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+```
+---
+
---
+++ d7/filesystem.py
@@ -0,0 +1,67 @@
+FILENAME = "test.input"
+
+def generate_list():
+    with open(FILENAME, "rt", encoding="ascii") as input_file:
+        reader = input_file.read().split("\n")
+        return reader
+
+
+class Directory:
+    name: str
+    size: int
+    contents: list
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.size = 0
+        self.contents = []
+
+    def add_child(self, child: object) -> None:
+        self.contents.append(child)
+
+    def get_size(self) -> int:
+        return self.size
+
+    def calc_size(self) -> int:
+        size = 0
+        if (len(self.contents) <= 1):
+            self.size = size
+            return size
+        else:
+            for x in self.contents:
+                child_size = x.get_size()
+                if (isinstance(x, Directory)):
+                    size += x.calc_size()
+                else:
+                    size += child_size
+            self.size = size
+            return size
+
+
+class File:
+    name: str
+    size: int
+
+    def __init__(self, name: str, size: int) -> None:
+        self.name = name
+        self.size = size
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_size(self) -> int:
+        return self.size
+
+
+def part_one():
+    terminal_output = generate_list()
+    dir_list = []
+
+
+def part_two():
+    pass
+
+
+if __name__ == "__main__":
+    part_one()
+    part_two()
