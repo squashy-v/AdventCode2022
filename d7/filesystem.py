c10d6f7032d1068142fe3882973032f46f46a2d5
squashy-v <jjamesvandergriff@gmail.com>
2024-02-28 17:22:41 -0500

    d7

--- d7/filesystem.py
+++ d7/filesystem.py
@@ -1,32 +1,20 @@
 from __future__ import annotations
-FILENAME = "filesystem.input"
+FILENAME: str = "filesystem.input"
 
 
-def generate_list():
+def generate_list() -> list:
     with open(FILENAME, "rt", encoding="ascii") as input_file:
-        reader = input_file.read().split("\n")
-        while "$ cd .." in reader:
-            reader.remove("$ cd ..")
+        reader = input_file.read().splitlines()
         while "$ ls" in reader:
-            reader.remove("$ ls")
-        return reader
+            reader.remove('$ ls')
+    return reader
 
 
 class Directory:
-    name: str
-    size: int
-    contents: list
-
-    def __init__(self, name: str) -> None:
-        self.name: str = name
+    def __init__(self, path: str) -> None:
         self.size: int = 0
         self.contents: list = []
-
-    def add_child(self, child: object) -> None:
-        self.contents.append(child)
-
-    def get_name(self) -> str:
-        return self.name
+        self.path = path
 
     def get_size(self) -> int:
         return self.size
@@ -34,87 +22,99 @@ class Directory:
     def get_contents(self) -> list:
         return self.contents
 
-    def calc_size(self) -> int:
-        size = 0
-        if (len(self.contents) < 1):
-            self.size = size
-            return size
-        else:
-            for x in self.contents:
-                if (isinstance(x, Directory)):
-                    size += x.calc_size()
-                else:
-                    child_size = x.get_size()
-                    size += child_size
-            self.size = size
-            return size
+    def get_path(self) -> str:
+        return self.path
 
-    def find_child(self, child: str) -> Directory:
+    def add_child_dir(self, dir: Directory) -> list:
+        self.contents.append(dir)
+        return self.contents
+
+    def add_child_file(self, size: int) -> int:
+        self.size += size
+        return self.size
+
+    def calc_size(self) -> int:
         for x in self.contents:
-            if (isinstance(x, Directory)):
-                if x.get_name() == child:
-                    return x
-                else:
-                    result = x.find_child(child)
-                    if isinstance(result, Directory):
-                        return result
+            self.size += x.calc_size()
+        return self.size
+
+    def find_child(self, child_path: str) -> Directory:
+        result = ""
+        for x in self.contents:
+            if x.get_path() == child_path:
+                result = x
+            else:
+                result = x.find_child(child_path)
+            if (isinstance(result, Directory)):
+                return result
 
 
 def dir_under_value(search_base: Directory, size: int) -> list:
     result: list = []
     for x in search_base.get_contents():
-        if (isinstance(x, Directory)):
-            if x.get_size() <= size:
-                result.append(x)
-            result.extend(dir_under_value(x, size))
+        if x.get_size() <= size:
+            result.append(x)
+        result.extend(dir_under_value(x, size))
     return result
 
 
-class File:
-    name: str
-    size: int
-
-    def __init__(self, name: str, size: int) -> None:
-        self.name: str = name
-        self.size: int = size
-
-    def get_name(self) -> str:
-        return self.name
-
-    def get_size(self) -> int:
-        return self.size
+def dir_over_value(search_base: Directory, size: int) -> list:
+    result: list = []
+    for x in search_base.get_contents():
+        if x.get_size() >= size:
+            result.append(x)
+        result.extend(dir_over_value(x, size))
+    return result
 
 
-def part_one() -> Directory:
+def part_one():
     terminal_output = generate_list()
     terminal_output.pop(0)
     root: Directory = Directory("/")
     cwd: Directory = root
-
     for i in terminal_output:
         if "$ cd" in i:
             new_cwd: str = i.replace("$ cd ", "")
-            cwd = root.find_child(new_cwd)
+            if new_cwd == "..":
+                new_cwd = cwd.get_path()
+                i = new_cwd.rfind("/", 0, -1)
+                new_cwd = new_cwd[:i+1]
+                if new_cwd == "/":
+                    cwd = root
+                else:
+                    cwd = root.find_child(new_cwd)
 
-        if "dir" in i:
+            else:
+                cwd = root.find_child(cwd.get_path() + new_cwd + "/")
+
+        elif "dir" in i:
             new_dir: str = i.replace("dir ", "")
-            cwd.add_child(Directory(new_dir))
+            cwd.add_child_dir(Directory(cwd.get_path() + new_dir + "/"))
 
-        if i[0].isnumeric():
-            new_file = i.split(" ")
-            cwd.add_child(File(new_file[1], int(new_file[0])))
+        elif i[0].isnumeric():
+            new_file: list = i.split(" ")
+            cwd.add_child_file(int(new_file[0]))
 
     root.calc_size()
     targets = dir_under_value(root, 100000)
-    targets = [x.size for x in targets]
+    targets = [x.get_size() for x in targets]
     print(sum(targets))
     return root
 
 
-def part_two():
-    pass
+def part_two(root: Directory):
+    size_free = 70000000 - root.get_size()
+    size_needed = 30000000 - size_free
+    targets = dir_over_value(root, size_needed)
+    targets = [x.get_size() for x in targets]
+    smallest_to_free = root.get_size()
+    for x in targets:
+        if (x < smallest_to_free):
+            smallest_to_free = x
+
+    print(smallest_to_free)
 
 
 if __name__ == "__main__":
-    part_one()
-    part_two()
+    root = part_one()
+    part_two(root)
